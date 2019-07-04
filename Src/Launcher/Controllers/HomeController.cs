651da5864091c14636d9898fe9ff139e4ae4e1c5
rtanev <rtanev@cleancodefactory.de>
2019-07-04 18:40:34 +0300

    Clean up unused variables

--- Src/Launcher/Controllers/HomeController.cs
+++ Src/Launcher/Controllers/HomeController.cs
@@ -1,22 +1,9 @@
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace Ccf.Ck.Launcher.Example.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _Logger;
-        private readonly IHostingEnvironment _HostingEnvironment;
-
-        public HomeController(ILogger<HomeController> logger, IHostingEnvironment hostingEnvironment)
-        {
-            _Logger = logger;
-            _HostingEnvironment = hostingEnvironment;
-        }
-
-        [Authorize]
         public IActionResult Index()
         {
             return View();
