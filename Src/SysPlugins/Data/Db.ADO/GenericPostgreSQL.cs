a9fa4aabd095f50b5a12f42ea1f026e80b7ab7da
Robert Tanev <rtanev@cleancodefactory.de>
2020-04-11 20:05:45 +0300

    Adding support for PostgreSQL

---
+++ Src/SysPlugins/Data/Db.ADO/GenericPostgreSQL.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Data.Sqlite;
+using Npgsql;
+
+namespace Ccf.Ck.SysPlugins.Data.Db.ADO
+{
+    public class GenericPostgreSQL : ADOBase<NpgsqlConnection> {
+        //public async override Task<IPluginsSynchronizeContextScoped> GetSynchronizeContextScopedAsync() {
+        //    return await Task.FromResult(new ADOSynchronizeContextScopedDefault<SqliteConnection>());
+        //}
+    }
+}
