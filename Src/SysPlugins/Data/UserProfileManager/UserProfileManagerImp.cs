a8687fd2c828ed76f9626f21c96da50298f3b21e
Robert Tanev <rtanev@cleancodefactory.de>
2020-06-11 16:00:50 +0300

    UserManager handles only the bearer token

--- Src/SysPlugins/Data/UserProfileManager/UserProfileManagerImp.cs
+++ Src/SysPlugins/Data/UserProfileManager/UserProfileManagerImp.cs
@@ -19,97 +19,45 @@ namespace Ccf.Ck.SysPlugins.Data.UserProfileManager
         protected override List<Dictionary<string, object>> Read(IDataLoaderReadContext execContext)
         {
             List<Dictionary<string, object>> result = new List<Dictionary<string, object>>();
-            string accessToken = GetAuthAccessToken(execContext);
             if (!string.IsNullOrEmpty(execContext.CurrentNode.Read.Select?.Query) && execContext.CurrentNode.Read.Select.Query.Equals("bearer", System.StringComparison.OrdinalIgnoreCase))
             {
+                string accessToken = GetAuthAccessToken(execContext);
+                KraftGlobalConfigurationSettings kraftGlobalConfigurationSettings = execContext.PluginServiceManager.GetService<KraftGlobalConfigurationSettings>(typeof(KraftGlobalConfigurationSettings));
+                string authority = kraftGlobalConfigurationSettings.GeneralSettings.Authority;
+                authority = authority.TrimEnd('/');
                 Dictionary<string, object> resultAuth = new Dictionary<string, object> ();
-                resultAuth.Add("key", GetAuthUrl(execContext, "avatar"));
+                resultAuth.Add("key", $"{authority}/api/avatar");
                 resultAuth.Add("token", accessToken);
                 resultAuth.Add("servicename", "avatarimage");
                 result.Add(resultAuth);
                 resultAuth = new Dictionary<string, object>();
-                resultAuth.Add("key", GetAuthUrl(execContext, "authority"));
+                resultAuth.Add("key", $"{authority}");
                 resultAuth.Add("token", accessToken);
                 resultAuth.Add("servicename", "authorizationserver");
                 result.Add(resultAuth);
+                resultAuth = new Dictionary<string, object>();
+                resultAuth.Add("key", $"{authority}/api/user");
+                resultAuth.Add("token", accessToken);
+                resultAuth.Add("servicename", "user");
+                result.Add(resultAuth);
             }
-            else
-            {
-                CancellationToken cancellationToken = new CancellationToken();
-                string targetUrl = GetAuthUrl(execContext, "select");
-                AuthenticationHeaderValue authenticationHeaderValue = new AuthenticationHeaderValue("Bearer", accessToken);
-                Task<Dictionary<string, object>> resultAuth =
-                    Loader.LoadAsync<Dictionary<string, object>>(cancellationToken, authenticationHeaderValue, HttpMethod.Get, null, targetUrl);
-                resultAuth.ConfigureAwait(false);
-                result.Add(resultAuth.Result);
-            }
+            //else
+            //{
+            //    CancellationToken cancellationToken = new CancellationToken();
+            //    string targetUrl = GetAuthUrl(execContext, "select");
+            //    AuthenticationHeaderValue authenticationHeaderValue = new AuthenticationHeaderValue("Bearer", accessToken);
+            //    Task<Dictionary<string, object>> resultAuth =
+            //        Loader.LoadAsync<Dictionary<string, object>>(cancellationToken, authenticationHeaderValue, HttpMethod.Get, null, targetUrl);
+            //    resultAuth.ConfigureAwait(false);
+            //    result.Add(resultAuth.Result);
+            //}
 
             return result;
         }
 
         protected override object Write(IDataLoaderWriteContext execContext)
-        {            
-            Dictionary<string, string> parameters = new Dictionary<string, string>();
-            parameters = MapParameters(execContext.Row);
-            List<Dictionary<string, object>> result = new List<Dictionary<string, object>>();
-            CancellationToken cancellationToken = new CancellationToken();
-            string targetUrl = GetAuthUrl(execContext, execContext.Operation);
-            AuthenticationHeaderValue authenticationHeaderValue = new AuthenticationHeaderValue("Bearer", GetAuthAccessToken(execContext));
-            Task<Dictionary<string, object>> resultAuth = Loader.LoadAsync<Dictionary<string, object>>(cancellationToken, authenticationHeaderValue, HttpMethod.Post, parameters, targetUrl);
-            resultAuth.ConfigureAwait(false);
-            result.Add(resultAuth.Result);
-            return result;
-        }
-
-        private Dictionary<string, string> MapParameters(Dictionary<string, object> row)
         {
-            Dictionary<string, string> result = new Dictionary<string, string>();
-            string given_name = row["given_name"] == null ? "null" : row["given_name"].ToString();
-            string family_name = row["family_name"] == null ? "null" : row["family_name"].ToString();
-            string email = row["email"] == null ? "null" : row["email"].ToString();
-            string phone_number = row["phone_number"] == null ? "null" : row["phone_number"].ToString();
-            string roles = row["roles"].ToString();
-            string communication_consent = row["communication_consent"] == null ? "false" : row["communication_consent"].ToString();
-
-            result.Add("given_name", given_name);
-            result.Add("family_name", family_name);
-            result.Add("email", email);
-            result.Add("phone_number", phone_number);
-            result.Add("roles", roles);
-            result.Add("communication_consent", communication_consent);
-
-            return result;
-        }
-
-        private string GetAuthUrl(IDataLoaderContext execContext, string action)
-        {
-            KraftGlobalConfigurationSettings kraftGlobalConfigurationSettings = execContext.PluginServiceManager.GetService<KraftGlobalConfigurationSettings>(typeof(KraftGlobalConfigurationSettings));
-            string authority = kraftGlobalConfigurationSettings.GeneralSettings.Authority;
-            authority = authority.TrimEnd('/');
-            switch (action) //select, insert, update, delete
-            {
-                case "select":
-                    {
-                        return $"{authority}/api/getuser";
-                    }
-                case "update":
-                    {
-                        return $"{authority}/api/updateuser";
-                    }
-                case "delete":
-                    {
-                        return $"{authority}/api/deleteuser";
-                    }
-                case "avatar":
-                    {
-                        return $"{authority}/api/avatar";
-                    }
-                case "authority":
-                    {
-                        return $"{authority}";
-                    }
-            }
-            return null;
+            throw new System.NotImplementedException();
         }
 
         private string GetAuthAccessToken(IDataLoaderContext execContext)
@@ -123,5 +71,72 @@ namespace Ccf.Ck.SysPlugins.Data.UserProfileManager
             }
             return accessTokenTask.Result;
         }
+
+        //protected override object Write(IDataLoaderWriteContext execContext)
+        //{            
+        //    Dictionary<string, string> parameters = new Dictionary<string, string>();
+        //    parameters = MapParameters(execContext.Row);
+        //    List<Dictionary<string, object>> result = new List<Dictionary<string, object>>();
+        //    CancellationToken cancellationToken = new CancellationToken();
+        //    string targetUrl = GetAuthUrl(execContext, execContext.Operation);
+        //    AuthenticationHeaderValue authenticationHeaderValue = new AuthenticationHeaderValue("Bearer", GetAuthAccessToken(execContext));
+        //    Task<Dictionary<string, object>> resultAuth = Loader.LoadAsync<Dictionary<string, object>>(cancellationToken, authenticationHeaderValue, HttpMethod.Post, parameters, targetUrl);
+        //    resultAuth.ConfigureAwait(false);
+        //    result.Add(resultAuth.Result);
+        //    return result;
+        //}
+
+        //private Dictionary<string, string> MapParameters(Dictionary<string, object> row)
+        //{
+        //    Dictionary<string, string> result = new Dictionary<string, string>();
+        //    string given_name = row["given_name"] == null ? "null" : row["given_name"].ToString();
+        //    string family_name = row["family_name"] == null ? "null" : row["family_name"].ToString();
+        //    string email = row["email"] == null ? "null" : row["email"].ToString();
+        //    string phone_number = row["phone_number"] == null ? "null" : row["phone_number"].ToString();
+        //    string roles = row["roles"].ToString();
+        //    string communication_consent = row["communication_consent"] == null ? "false" : row["communication_consent"].ToString();
+
+        //    result.Add("given_name", given_name);
+        //    result.Add("family_name", family_name);
+        //    result.Add("email", email);
+        //    result.Add("phone_number", phone_number);
+        //    result.Add("roles", roles);
+        //    result.Add("communication_consent", communication_consent);
+
+        //    return result;
+        //}
+
+        //private string GetAuthUrl(IDataLoaderContext execContext, string action)
+        //{
+        //    KraftGlobalConfigurationSettings kraftGlobalConfigurationSettings = execContext.PluginServiceManager.GetService<KraftGlobalConfigurationSettings>(typeof(KraftGlobalConfigurationSettings));
+        //    string authority = kraftGlobalConfigurationSettings.GeneralSettings.Authority;
+        //    authority = authority.TrimEnd('/');
+        //    switch (action) //select, insert, update, delete
+        //    {
+        //        case "select":
+        //            {
+        //                return $"{authority}/api/getuser";
+        //            }
+        //        case "update":
+        //            {
+        //                return $"{authority}/api/updateuser";
+        //            }
+        //        case "delete":
+        //            {
+        //                return $"{authority}/api/deleteuser";
+        //            }
+        //        case "avatar":
+        //            {
+        //                return $"{authority}/api/avatar";
+        //            }
+        //        case "authority":
+        //            {
+        //                return $"{authority}";
+        //            }
+        //    }
+        //    return null;
+        //}
+
+
     }
 }
