7db324f793c4d2a355c545db8fe66ee6ed9ff1e9
Robert Tanev <rtanev@cleancodefactory.de>
2020-10-22 16:45:20 +0300

    Adding construct for request recorder

---
+++ Src/SysPlugins/Interfaces/IRequestRecorder.cs
@@ -0,0 +1,16 @@
+﻿using Ccf.Ck.Models.NodeRequest;
+using Microsoft.AspNetCore.Http;
+using Org.BouncyCastle.Asn1.Ocsp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ccf.Ck.SysPlugins.Interfaces
+{
+    public interface IRequestRecorder
+    {
+        Task HandleRequest(HttpRequest request, InputModel inputModel);
+        Task<StringBuilder> GetFinalResult();
+    }
+}
---
+++ Src/SysPlugins/Recorders/Postman/PostmanImp.cs
@@ -0,0 +1,23 @@
+﻿using Ccf.Ck.Models.NodeRequest;
+using Ccf.Ck.SysPlugins.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ccf.Ck.SysPlugins.Recorders.Postman
+{
+    public class PostmanImp : IRequestRecorder
+    {
+        public Task<StringBuilder> GetFinalResult()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task HandleRequest(HttpRequest request, InputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
