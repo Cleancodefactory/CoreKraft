6e9b6f32e373ba3255806821637a3861578098a1
Robert Tanev <rtanev@cleancodefactory.de>
2020-11-18 21:24:50 +0200

    Adding a general svg tag helper

---
+++ Src/Launchers/Main/TagHelpers/SvgEmbedTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ccf.Ck.Launchers.Main.TagHelpers
+{
+    public class SvgEmbedTagHelper : TagHelper
+    {
+        private IWebHostEnvironment _WebHostEnvironment;
+        public SvgEmbedTagHelper(IWebHostEnvironment env)
+        {
+
+        }
+        public string RelativePath { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            //<svgembed relative-path="/images/landing/share_screen.svg"></svgembed>
+            output.Content.SetContent(File.ReadAllText(Path.Combine(_WebHostEnvironment.ContentRootPath, RelativePath)));
+        }
+    }
+}
