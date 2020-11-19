2ad69731540059e5ca490235485b6978d93bb953
Robert Tanev <rtanev@cleancodefactory.de>
2020-11-19 19:10:53 +0200

    Finished the svg tag helper

--- Src/Launchers/Main/TagHelpers/SvgEmbedTagHelper.cs
+++ Src/Launchers/Main/TagHelpers/SvgEmbedTagHelper.cs
@@ -8,19 +8,29 @@ using System.Threading.Tasks;
 
 namespace Ccf.Ck.Launchers.Main.TagHelpers
 {
+    [HtmlTargetElement("svgembed")]
     public class SvgEmbedTagHelper : TagHelper
     {
         private IWebHostEnvironment _WebHostEnvironment;
+        private string _RelativePath;
+
         public SvgEmbedTagHelper(IWebHostEnvironment env)
         {
             _WebHostEnvironment = env;
         }
-        public string RelativePath { get; set; }
+        public string RelativePath
+        {
+            get {
+                return _RelativePath.TrimStart('/').TrimStart('\\'); 
+            }
+            set { _RelativePath = value; }
+        }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             //<svgembed relative-path="/images/landing/share_screen.svg"></svgembed>
-            output.Content.SetContent(File.ReadAllText(Path.Combine(_WebHostEnvironment.ContentRootPath, RelativePath)));
+            output.TagName = null;
+            output.Content.SetHtmlContent(File.ReadAllText(Path.Combine(_WebHostEnvironment.WebRootPath, RelativePath)));
         }
     }
 }
