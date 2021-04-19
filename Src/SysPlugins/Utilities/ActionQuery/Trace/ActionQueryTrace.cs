bca552e7748f07d8b47b56072571efea0c36f25a
Michael Palazov <mpalazov@cleancodefactory.de>
2021-04-19 14:17:25 +0300

    ...

--- Src/SysPlugins/Utilities/ActionQuery/Trace/ActionQueryTrace.cs
+++ Src/SysPlugins/Utilities/ActionQuery/Trace/ActionQueryTrace.cs
@@ -1,13 +1,21 @@
 ﻿using Ccf.Ck.Libs.ActionQuery;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Ccf.Ck.SysPlugins.Utilities
 {
     public class ActionQueryTrace
     {
+        public ActionQueryTrace(IEnumerable<Instruction> program, int steplimit)
+        {
+            _program = program.ToArray();
+            _steps = new Queue<ActionQueryStep>(steplimit+1);
+        }
+
         #region Raw data
+        private int _steplimit = 10;
         private Instruction[] _program;
 
         public IEnumerable<Instruction> Program
@@ -18,6 +26,23 @@ namespace Ccf.Ck.SysPlugins.Utilities
             }
         }
 
+        private Queue<ActionQueryStep> _steps = null;
+
+        public void AddStep(ActionQueryStep step)
+        {
+            _steps.Enqueue(step);
+            if (_steps.Count > _steplimit)
+            {
+                _steps.Dequeue();
+            }
+        }
+        public IEnumerable<ActionQueryStep> Steps
+        {
+            get
+            {
+                return _steps.Reverse().ToArray();
+            }
+        }
         #endregion
     }
 }
