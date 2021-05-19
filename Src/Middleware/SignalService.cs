69ed1fa7c9ac8a82326a142ea744ca5689354e34
Robert Tanev <rtanev@cleancodefactory.de>
2021-05-19 19:14:48 +0300

    Fix bug with FileSystemWatcher. Now a hosting service is keeping the references alive.

--- Src/Middleware/SignalService.cs
+++ Src/Middleware/SignalService.cs
@@ -5,8 +5,10 @@ using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using static Ccf.Ck.Utilities.Generic.Utilities;
 
 namespace Ccf.Ck.Web.Middleware
 {
@@ -14,12 +16,14 @@ namespace Ccf.Ck.Web.Middleware
     {
         private readonly List<Timer> _Timers;
         private readonly IServiceScopeFactory _ScopeFactory;
+        private List<FileSystemWatcher> _FileWatchers;
         static readonly object _Lock = new object();
 
         public SignalService(IServiceScopeFactory scopeFactory)
         {
             _ScopeFactory = scopeFactory;
             _Timers = new List<Timer>();
+            _FileWatchers = new List<FileSystemWatcher>(100);
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
@@ -72,6 +76,71 @@ namespace Ccf.Ck.Web.Middleware
             foreach (Timer timer in _Timers)
             {
                 timer?.Dispose();
+            }
+            foreach (FileSystemWatcher fileSystemWatcher in _FileWatchers)
+            {
+                fileSystemWatcher.EnableRaisingEvents = false;
+                fileSystemWatcher.Dispose();
+            }
+        }
+
+        public void AttachModulesWatcher(string moduleFolder, 
+            bool includeSubdirectories, 
+            IHostApplicationLifetime applicationLifetime, 
+            Action<bool> restart,
+            UnhandledExceptionEventHandler appDomain_OnUnhandledException, 
+            ResolveEventHandler appDomain_OnAssemblyResolve)
+        {
+            if (!Directory.Exists(moduleFolder))
+            {
+                //Do nothing for none existant folders
+                return;
+            }
+            FileSystemWatcher fileWatcher;
+            RestartReason restartReason = new RestartReason();
+            fileWatcher = new FileSystemWatcher(moduleFolder)
+            {
+                // watch for everything
+                NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName,
+                IncludeSubdirectories = includeSubdirectories,
+                Filter = "*.*",
+                InternalBufferSize = 16384
+            };
+            // Add event handlers.
+            fileWatcher.Changed += OnChanged;
+            fileWatcher.Created += OnChanged;
+            fileWatcher.Deleted += OnChanged;
+            fileWatcher.Renamed += OnRenamed;
+            fileWatcher.Error += OnError;
+
+            // Begin watching...
+            fileWatcher.EnableRaisingEvents = true;
+            _FileWatchers.Add(fileWatcher);
+
+            void OnChanged(object source, FileSystemEventArgs e)
+            {
+                fileWatcher.EnableRaisingEvents = false;
+                //Bug in Docker which will trigger OnChanged during StartUp (How to fix?)
+                AppDomain.CurrentDomain.UnhandledException -= appDomain_OnUnhandledException;
+                AppDomain.CurrentDomain.AssemblyResolve -= appDomain_OnAssemblyResolve;
+                restartReason.Reason = "File Changed";
+                restartReason.Description = $"ChangeType: {e.ChangeType} file {e.FullPath}";
+                RestartApplication(applicationLifetime, restartReason, restart);
+            }
+
+            void OnRenamed(object source, RenamedEventArgs e)
+            {
+                fileWatcher.EnableRaisingEvents = false;
+                AppDomain.CurrentDomain.UnhandledException -= appDomain_OnUnhandledException;
+                AppDomain.CurrentDomain.AssemblyResolve -= appDomain_OnAssemblyResolve;
+                restartReason.Reason = "File Renamed";
+                restartReason.Description = $"Renaming from {e.OldFullPath} to {e.FullPath}";
+                RestartApplication(applicationLifetime, restartReason, restart);
+            }
+
+            void OnError(object sender, ErrorEventArgs e)
+            {
+                KraftLogger.LogCritical(e.GetException(), "OnError in AttachModulesWatcher");
             }
         }
     }
