9578ab7d25fff69b22d0e97e9b3302d3153365a8
Robert Tanev <rtanev@cleancodefactory.de>
2021-07-01 20:19:04 +0300

    Introduce RazorCatchAll

--- Src/Models/Settings/RazorAreaAssemblySettings.cs
+++ Src/Models/Settings/RazorAreaAssemblySettings.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Ccf.Ck.Models.Settings
 {
@@ -27,5 +28,34 @@ namespace Ccf.Ck.Models.Settings
         public List<string> AssemblyNamesView { get; set; }
         public string DefaultRouting { get; set; }
         public List<RouteMapping> RouteMappings { get; set; }
+        public string CatchAllRouting { get; set; }
+
+        public RazorCatchAll ParseRazorCatchAll()
+        {
+            RazorCatchAll razorCatchAll = new RazorCatchAll();
+            if (!string.IsNullOrEmpty(CatchAllRouting))
+            {
+                string pattern = @"^\{controller=(.*)\}\/\{action=(.*?)\}(?:\/\{(.*?)[\?]?\})?";
+                RegexOptions options = RegexOptions.Singleline;
+
+                Match match = Regex.Match(CatchAllRouting, pattern, options);
+
+                if (match.Success)
+                {
+                    razorCatchAll.CatchAllAction = match.Groups[2].Value;
+                    razorCatchAll.CatchAllController = match.Groups[1].Value;
+                    razorCatchAll.CatchAllParameter = match.Groups[3].Success ? match.Groups[3].Value : "id";
+                    return razorCatchAll;
+                }
+            }
+            return null;
+        }
+    }
+
+    public class RazorCatchAll
+    {
+        public string CatchAllAction { get; set; }
+        public string CatchAllController { get; set; }
+        public string CatchAllParameter { get; set; }
     }
 }
