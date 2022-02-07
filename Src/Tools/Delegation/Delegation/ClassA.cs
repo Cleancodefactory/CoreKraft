3408f0a73f90ee20a39cf77a57c8592376376085
Michael Palazov <mpalazov@cleancodefactory.de>
2022-02-07 20:34:47 +0200

    A test program

---
+++ Src/Tools/Delegation/Delegation/ClassA.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegation
+{
+
+    public delegate void DS(ClassA _this, string arg);
+
+    public class ClassA
+    {
+        public static Dictionary<string, DS> _cache = new Dictionary<string, DS>();
+
+        public string _instanceName = null;
+        public ClassA(string iname)
+        {
+            _instanceName = iname;
+        }
+        static ClassA()
+        {
+            var t = typeof(ClassA);
+            var mi1 = t.GetMethod("M1");
+            _cache.Add("M1", mi1.CreateDelegate<DS>(null));
+            mi1 = t.GetMethod("M2");
+            _cache.Add("M2", mi1.CreateDelegate<DS>(null));
+        }
+
+        public void Call(string method)
+        {
+            if (_cache.ContainsKey(method))
+            {
+                var d = _cache[method];
+                d(this, "sdfsdF");
+                return;
+            }
+            Console.WriteLine("Method not found");
+        }
+
+
+
+
+
+        public void M1(string arg)
+        {
+            //Console.WriteLine("M1:{0}", _instanceName);
+        }
+        public void M2(string arg)
+        {
+            //Console.WriteLine("M2:{0}", _instanceName);
+        }
+    }
+}
---
+++ Src/Tools/Delegation/Delegation/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Delegation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ClassA a = new ClassA("A");
+            ClassA b = new ClassA("B");
+            Stopwatch sv = new Stopwatch();
+            sv.Start();
+            for (var i = 0; i < 10000; i++)
+            {
+                a.Call("M1");
+                b.Call("M1");
+            }
+            sv.Stop();
+            Console.WriteLine("Ellapsed {0}", sv.ElapsedMilliseconds);
+
+            sv = new Stopwatch();
+            sv.Start();
+            for (var i = 0; i < 1000000; i++)
+            {
+                a.Call("M1");
+                b.Call("M1");
+            }
+            sv.Stop();
+            Console.WriteLine("Ellapsed {0}", sv.ElapsedMilliseconds);
+
+            sv = new Stopwatch();
+            sv.Start();
+            for (var i = 0; i < 1000000; i++)
+            {
+                a.M1("A");
+                b.M1("B");
+            }
+            sv.Stop();
+            Console.WriteLine("Ellapsed {0}", sv.ElapsedMilliseconds);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
