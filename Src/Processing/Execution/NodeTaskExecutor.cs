fd8d9fd71f18076d102c4832925b6b3483210b92
Robert Tanev <rtanev@cleancodefactory.de>
2022-05-04 11:03:39 +0300

    Implement better error handling. Show nodeset, nodekey and parameters at the time of exception

--- Src/Processing/Execution/NodeTaskExecutor.cs
+++ Src/Processing/Execution/NodeTaskExecutor.cs
@@ -11,6 +11,8 @@ using Ccf.Ck.SysPlugins.Interfaces.ContextualBasket;
 using Ccf.Ck.SysPlugins.Interfaces.NodeExecution;
 using static Ccf.Ck.SysPlugins.Interfaces.Packet.StatusResultEnum;
 using System.Threading.Tasks;
+using System.Text;
+using System.Linq;
 
 namespace Ccf.Ck.Processing.Execution
 {
@@ -39,36 +41,21 @@ namespace Ccf.Ck.Processing.Execution
                 GenericUtilities.CheckNullOrEmpty(processingContext, true);
                 GenericUtilities.CheckNullOrEmpty(_TransactionScope.PluginServiceManager, true);
             }
-            try
+            if (processingContext.InputModel.LoaderType.HasFlag(ELoaderType.DataLoader))
             {
-                if (processingContext.InputModel.LoaderType.HasFlag(ELoaderType.DataLoader))
+                using (KraftProfiler.Current.Step("Execution time loading data: "))
                 {
-                    using (KraftProfiler.Current.Step("Execution time loading data: "))
+                    if (loaderContextDefinition.StartNode != null /*&& loaderContextDefinition.StartNode.HasValidDataSection(processingContext.InputModel.IsWriteOperation)*/)
                     {
-                        if (loaderContextDefinition.StartNode != null /*&& loaderContextDefinition.StartNode.HasValidDataSection(processingContext.InputModel.IsWriteOperation)*/)
-                        {
-                            IDataIteratorPlugin dataIteratorPlugin = Utilities.GetPlugin<IDataIteratorPlugin>(_KraftModuleConfigurationSettings.NodeSetSettings.SourceLoaderMapping.NodesDataIterator.NodesDataIteratorConf.Name, _TransactionScope.DependencyInjectionContainer, _KraftModuleConfigurationSettings, ELoaderType.DataLoader, true);
-                            GenericUtilities.CheckNullOrEmpty(dataIteratorPlugin, true);
-                            IProcessingContext r = dataIteratorPlugin.ExecuteAsync(loaderContextDefinition, processingContext, _TransactionScope.PluginServiceManager, dataLoaderAccessor, pluginAccessor).Result;
-                        }
+                        IDataIteratorPlugin dataIteratorPlugin = Utilities.GetPlugin<IDataIteratorPlugin>(_KraftModuleConfigurationSettings.NodeSetSettings.SourceLoaderMapping.NodesDataIterator.NodesDataIteratorConf.Name, _TransactionScope.DependencyInjectionContainer, _KraftModuleConfigurationSettings, ELoaderType.DataLoader, true);
+                        GenericUtilities.CheckNullOrEmpty(dataIteratorPlugin, true);
+                        IProcessingContext r = dataIteratorPlugin.ExecuteAsync(loaderContextDefinition, processingContext, _TransactionScope.PluginServiceManager, dataLoaderAccessor, pluginAccessor).Result;
                     }
                 }
-                if (!_CollectiveCall)
-                {
-                    _TransactionScope.CommitTransactions();
-                }
             }
-            catch (Exception ex)
+            if (!_CollectiveCall)
             {
-                processingContext.ReturnModel.Status.IsSuccessful = false;
-                processingContext.ReturnModel.Status.StatusResults.Add(new StatusResult { StatusResultType = EStatusResult.StatusResultError, Message = ex.Message });
-                if (!_CollectiveCall)
-                {
-                    _TransactionScope.RollbackTransactions();
-                }
-                KraftLogger.LogError(ex.Message, ex);
-
-                throw; //Throw to aniliate the _CollectiveCall break
+                _TransactionScope.CommitTransactions();
             }
         }
 
@@ -84,37 +71,24 @@ namespace Ccf.Ck.Processing.Execution
                 GenericUtilities.CheckNullOrEmpty(_TransactionScope.PluginServiceManager, true);
             }
 
-            try
+            using (KraftProfiler.Current.Step("Execution time loading views: "))
             {
-                using (KraftProfiler.Current.Step("Execution time loading views: "))
+                if (!string.IsNullOrEmpty(processingContext.InputModel.BindingKey))
                 {
-                    if (!string.IsNullOrEmpty(processingContext.InputModel.BindingKey))
+                    View view = loaderContextDefinition.StartNode.Views.Find(v => v.BindingKey.Equals(processingContext.InputModel.BindingKey, StringComparison.OrdinalIgnoreCase));
+                    ExecuteNodeViewPrivate(loaderContextDefinition, processingContext, view);
+                }
+                else
+                {
+                    foreach (View view in loaderContextDefinition.StartNode.Views)
                     {
-                        View view = loaderContextDefinition.StartNode.Views.Find(v => v.BindingKey.Equals(processingContext.InputModel.BindingKey, StringComparison.OrdinalIgnoreCase));
                         ExecuteNodeViewPrivate(loaderContextDefinition, processingContext, view);
                     }
-                    else
-                    {
-                        foreach (View view in loaderContextDefinition.StartNode.Views)
-                        {
-                            ExecuteNodeViewPrivate(loaderContextDefinition, processingContext, view);
-                        }
-                    }                    
-                }
-                if (!_CollectiveCall)
-                {
-                    _TransactionScope.CommitTransactions();
                 }
             }
-            catch (Exception ex)
+            if (!_CollectiveCall)
             {
-                processingContext.ReturnModel.Status.IsSuccessful = false;
-                processingContext.ReturnModel.Status.StatusResults.Add(new StatusResult { StatusResultType = EStatusResult.StatusResultError, Message = ex.Message });
-                if (!_CollectiveCall)
-                {
-                    _TransactionScope.RollbackTransactions();
-                }
-                KraftLogger.LogError(ex.Message, ex);
+                _TransactionScope.CommitTransactions();
             }
         }
 
@@ -131,8 +105,8 @@ namespace Ccf.Ck.Processing.Execution
         }
 
         public void Execute(
-            LoadedNodeSet loaderContextDefinition, 
-            IProcessingContext processingContext, 
+            LoadedNodeSet loaderContextDefinition,
+            IProcessingContext processingContext,
             IPluginAccessor<IDataLoaderPlugin> dataLoaderAccessor,
             IPluginAccessor<INodePlugin> pluginAccessor)
         {
@@ -153,7 +127,7 @@ namespace Ccf.Ck.Processing.Execution
                 {
                     ExecuteNodeView(loaderContextDefinition, processingContext);
                 }
-                
+
                 //if (processingContext.InputModel.LoaderType.HasFlag(ELoaderType.LookupLoader))
                 //{
                 //    if (loaderContextDefinition.StartNode != null && loaderContextDefinition.StartNode.HasLookup())
@@ -176,10 +150,27 @@ namespace Ccf.Ck.Processing.Execution
             }
             catch (Exception ex)
             {
+
                 processingContext.ReturnModel.Status.IsSuccessful = false;
                 processingContext.ReturnModel.Status.StatusResults.Add(new StatusResult { StatusResultType = EStatusResult.StatusResultError, Message = ex.Message });
                 _TransactionScope.RollbackTransactions();
-                KraftLogger.LogError(ex.Message, ex);
+
+                StringBuilder errMsg = new StringBuilder(1000);
+                errMsg.AppendLine($"Nodeset: {loaderContextDefinition.NodeSet.Name}");
+                errMsg.AppendLine($"Startnodekey: {loaderContextDefinition.StartNode.NodeKey}");
+                errMsg.AppendLine($"Parameters: {string.Join(",", loaderContextDefinition.StartNode.Parameters)}");
+                if (loaderContextDefinition.StartNode.CollectedReadParameters != null)
+                {
+                    errMsg.AppendLine($"CollectedReadParameters: {string.Join(",", loaderContextDefinition.StartNode.CollectedReadParameters.Select(p => p.Name))}");
+                }
+                if (loaderContextDefinition.StartNode.CollectedWriteParameters != null)
+                {
+                    errMsg.AppendLine($"CollectedWriteParameters: {string.Join(",", loaderContextDefinition.StartNode.CollectedWriteParameters.Select(p => p.Name))}");
+                }
+                errMsg.AppendLine($"DataPluginName: {loaderContextDefinition.StartNode.DataPluginName}");
+                errMsg.AppendLine($"RequireAuthentication: {loaderContextDefinition.StartNode.RequireAuthentication}");
+                errMsg.AppendLine($"Error.Message: {ex.Message}");
+                KraftLogger.LogError(errMsg.ToString(), ex);
             }
             finally
             {
