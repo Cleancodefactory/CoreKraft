9348736a871c4a8fc4989bdae883ee8107c2f33a
Michael Palazov <mpalazov@cleancodefactory.de>
2022-07-04 21:40:15 +0300

    ...

---
+++ Src/Models/Enumerations/ECallType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ccf.Ck.Models.Enumerations {
+    /// <summary>
+    /// Describes the kind of request being processed - web request, direct call, signal etc.
+    /// </summary>
+    public enum ECallType {
+        WebRequest = 0,
+        DirectCall = 1,
+        Signal = 2
+    }
+}
---
+++ Src/Models/NodeRequest/ServerModel.cs
@@ -0,0 +1,17 @@
+﻿using Ccf.Ck.Models.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ccf.Ck.Models.NodeRequest {
+    public class ServerModel {
+
+        public Dictionary<string, object> Variables { get; init; } = new Dictionary<string, object>();
+
+        public ECallType CallType { get; set; }
+
+
+    }
+}
