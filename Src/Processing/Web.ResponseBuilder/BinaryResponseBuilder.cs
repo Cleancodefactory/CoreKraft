e46726f49c7bb56a9888c1bbbd7c13debfe44135
Robert Tanev <rtanev@cleancodefactory.de>
2024-05-10 18:15:10 +0300

    Handle RangeItemHeaderValue for videos

--- Src/Processing/Web.ResponseBuilder/BinaryResponseBuilder.cs
+++ Src/Processing/Web.ResponseBuilder/BinaryResponseBuilder.cs
@@ -1,6 +1,9 @@
-﻿using Ccf.Ck.Models.NodeRequest;
-using Ccf.Ck.Models.ContextBasket;
+﻿using Ccf.Ck.Models.ContextBasket;
+using Ccf.Ck.Models.NodeRequest;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -9,7 +12,7 @@ namespace Ccf.Ck.Processing.Web.ResponseBuilder
     public class BinaryResponseBuilder : HttpResponseBuilder
     {
 
-        public BinaryResponseBuilder(ProcessingContextCollection processingContextCollection):base(processingContextCollection)
+        public BinaryResponseBuilder(ProcessingContextCollection processingContextCollection) : base(processingContextCollection)
         {
         }
 
@@ -28,12 +31,13 @@ namespace Ccf.Ck.Processing.Web.ResponseBuilder
             }
         }
 
-        //response.ContentType = "image"; Coming from SendFileAsync
-
         protected override void WriteToResponseBody(HttpContext context)
         {
+            HttpResponse response = context.Response;
+            HttpRequest request = context.Request;
+
             //Etag controls it and we don't need body
-            if (context.Response.StatusCode == StatusCodes.Status304NotModified)
+            if (response.StatusCode == StatusCodes.Status304NotModified)
             {
                 return;
             }
@@ -41,14 +45,67 @@ namespace Ccf.Ck.Processing.Web.ResponseBuilder
             {
                 if (!string.IsNullOrWhiteSpace(postedFile.ContentType))
                 {
-                    context.Response.ContentType = postedFile.ContentType;
+                    response.ContentType = postedFile.ContentType;
                 }
-                using (var pfs = postedFile.OpenReadStream()) {
 
-                    pfs.CopyTo(context.Response.Body);
+                response.ContentLength = postedFile.Length;
+
+                //We can decide to split only big videos into ranges
+                //if (postedFile.ContentType.Equals("video/mp4", comparisonType: StringComparison.OrdinalIgnoreCase))
+                //{
+
+                //}               
+                response.Headers.ContentRange = $"bytes */{postedFile.Length}";
+
+                var ranges = request.GetTypedHeaders().Range?.Ranges;
+
+                if (ranges != null && ranges.Count == 1 && ranges.First().From.HasValue && ranges.First().To.HasValue)
+                {
+                    RangeItemHeaderValue range = ranges.First();
+
+                    // Set Content-Range header
+                    response.Headers.Add("Content-Range", $"bytes {range.From}-{range.To}/{postedFile.ContentType.Length}");
+                    // Set status code to 206 Partial Content
+                    response.StatusCode = StatusCodes.Status206PartialContent;
+                    // Set content length for the range
+                    response.ContentLength = range.To - range.From + 1;
+
+                    byte[] data = ReadStreamIntoByteArray(postedFile.OpenReadStream(), range.From.Value, range.To.Value);
+                    // Write the specified range of binary data to the response
+                    response.Body.Write(data, (int)range.From, (int)range.To.Value);
                 }
-                //context.Response.SendFileAsync(postedFile.FileName).Wait();
+                else
+                {
+                    response.StatusCode = StatusCodes.Status200OK;
+                    using (System.IO.Stream pfs = postedFile.OpenReadStream())
+                    {
+                        pfs.CopyTo(response.Body);
+                    }
+                }
             }
+        }
+
+        private static byte[] ReadStreamIntoByteArray(Stream stream, long from, long to)
+        {
+            if (from < 0 || to < from || stream == null || !stream.CanSeek || !stream.CanRead)
+            {
+                throw new ArgumentException("Invalid arguments");
+            }
+
+            byte[] buffer = new byte[to - from + 1];
+
+            stream.Seek(from, SeekOrigin.Begin);
+
+            int bytesRead = 0;
+            int totalBytesRead = 0;
+
+            while (totalBytesRead < buffer.Length &&
+                   (bytesRead = stream.Read(buffer, totalBytesRead, buffer.Length - totalBytesRead)) > 0)
+            {
+                totalBytesRead += bytesRead;
+            }
+
+            return buffer;
         }
     }
 }
