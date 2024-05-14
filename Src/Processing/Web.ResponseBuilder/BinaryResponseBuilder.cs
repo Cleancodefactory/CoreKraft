4766f568e0a90d07f88db3c5665bfa1a3f2509f5
Robert Tanev <rtanev@cleancodefactory.de>
2024-05-14 14:26:02 +0300

    ...

--- Src/Processing/Web.ResponseBuilder/BinaryResponseBuilder.cs
+++ Src/Processing/Web.ResponseBuilder/BinaryResponseBuilder.cs
@@ -1,18 +1,17 @@
 ﻿using Ccf.Ck.Models.ContextBasket;
 using Ccf.Ck.Models.NodeRequest;
-using Grace.DependencyInjection;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Net.Http.Headers;
 using System;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Ccf.Ck.Processing.Web.ResponseBuilder
 {
     public class BinaryResponseBuilder : HttpResponseBuilder
     {
-
         public BinaryResponseBuilder(ProcessingContextCollection processingContextCollection) : base(processingContextCollection)
         {
         }
@@ -20,41 +19,21 @@ namespace Ccf.Ck.Processing.Web.ResponseBuilder
         protected override void WriteToResponseHeaders(HttpContext context)
         {
             HttpResponse response = context.Response;
-            //image / jpeg
             if (_ProcessingContextCollection.ProcessingContexts.First().ReturnModel.BinaryData is IPostedFile postedFile)
             {
                 if (!string.IsNullOrWhiteSpace(postedFile.ContentType))
                 {
-                    //For known content types create the etag
                     string etag = Ccf.Ck.Utilities.Generic.Utilities.GenerateETag(Encoding.UTF8.GetBytes(postedFile.Length + postedFile.FileName));
                     CacheManagement.HandleEtag(context, etag);
                 }
             }
         }
 
-        private RequestType _requestType;
-
         protected override void WriteToResponseBody(HttpContext context)
         {
             HttpResponse response = context.Response;
             HttpRequest request = context.Request;
-            string _method = request.Method;
 
-            if (HttpMethods.IsGet(_method))
-            {
-                _requestType = RequestType.IsGet;
-            }
-            else if (HttpMethods.IsHead(_method))
-            {
-                _requestType = RequestType.IsHead;
-            }
-            else
-            {
-                _requestType = RequestType.Unspecified;
-            }
-
-
-            //Etag controls it and we don't need body
             if (response.StatusCode == StatusCodes.Status304NotModified)
             {
                 return;
@@ -66,46 +45,45 @@ namespace Ccf.Ck.Processing.Web.ResponseBuilder
                     response.ContentType = postedFile.ContentType;
                 }
 
-                response.ContentLength = postedFile.Length;            
+                response.ContentLength = postedFile.Length;
                 response.Headers.AcceptRanges = "bytes";
-                response.Headers.ContentRange = $"bytes */{postedFile.Length}";
 
                 var ranges = request.GetTypedHeaders().Range?.Ranges;
 
                 if (ranges != null && ranges.Count == 1 && ranges.First().From.HasValue && ranges.First().To.HasValue)
                 {
-                    RangeItemHeaderValue range = ranges.First();
-
-                    // Set Content-Range header
-                    if (response.Headers.ContainsKey("Content-Range"))
-                    {
-                        response.Headers["Content-Range"] = $"bytes {range.From}-{range.To}/{postedFile.ContentType.Length}";
-                    }
-                    else
-                    {
-                        response.Headers.Add("Content-Range", $"bytes {range.From}-{range.To}/{postedFile.ContentType.Length}");
-                    }
-                    // Set status code to 206 Partial Content
-                    response.StatusCode = StatusCodes.Status206PartialContent;
-                    // Set content length for the range
-                    response.ContentLength = range.To - range.From + 1;
-
-                    byte[] data = ReadStreamIntoByteArray(postedFile.OpenReadStream(), range.From.Value, range.To.Value);
-                    // Write the specified range of binary data to the response
-                    response.Body.Write(data, (int)range.From, (int)range.To.Value);
+                    WriteRangeResponseAsync(response, postedFile, ranges.First()).Wait();
                 }
                 else
                 {
-                    response.StatusCode = StatusCodes.Status200OK;
-                    using (System.IO.Stream pfs = postedFile.OpenReadStream())
-                    {
-                        pfs.CopyTo(response.Body);
-                    }
+                    WriteFullResponseAsync(response, postedFile).Wait();
                 }
             }
         }
 
-        private static byte[] ReadStreamIntoByteArray(Stream stream, long from, long to)
+        private async Task WriteRangeResponseAsync(HttpResponse response, IPostedFile postedFile, RangeItemHeaderValue range)
+        {
+            long from = range.From!.Value;
+            long to = range.To!.Value;
+
+            response.Headers.ContentRange = $"bytes {from}-{to}/{postedFile.Length}";
+            response.StatusCode = StatusCodes.Status206PartialContent;
+            response.ContentLength = to - from + 1;
+
+            byte[] data = await ReadStreamIntoByteArrayAsync(postedFile.OpenReadStream(), from, to);
+            await response.Body.WriteAsync(data, 0, data.Length);
+        }
+
+        private async Task WriteFullResponseAsync(HttpResponse response, IPostedFile postedFile)
+        {
+            response.StatusCode = StatusCodes.Status200OK;
+            using (Stream stream = postedFile.OpenReadStream())
+            {
+                await stream.CopyToAsync(response.Body);
+            }
+        }
+
+        private static async Task<byte[]> ReadStreamIntoByteArrayAsync(Stream stream, long from, long to)
         {
             if (from < 0 || to < from || stream == null || !stream.CanSeek || !stream.CanRead)
             {
@@ -120,22 +98,12 @@ namespace Ccf.Ck.Processing.Web.ResponseBuilder
             int totalBytesRead = 0;
 
             while (totalBytesRead < buffer.Length &&
-                   (bytesRead = stream.Read(buffer, totalBytesRead, buffer.Length - totalBytesRead)) > 0)
+                   (bytesRead = await stream.ReadAsync(buffer, totalBytesRead, buffer.Length - totalBytesRead)) > 0)
             {
                 totalBytesRead += bytesRead;
             }
 
             return buffer;
-        }
-
-
-        [Flags]
-        private enum RequestType : byte
-        {
-            Unspecified = 0b_000,
-            IsHead = 0b_001,
-            IsGet = 0b_010,
-            IsRange = 0b_100,
         }
     }
 }
