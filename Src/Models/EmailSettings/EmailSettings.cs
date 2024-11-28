2745d854d86dc752226c247cd78b339261a82cc5
Robert Tanev <rtanev@cleancodefactory.de>
2024-11-28 16:59:42 +0200

    Updated nuget packages

--- Src/Models/EmailSettings/EmailSettings.cs
+++ Src/Models/EmailSettings/EmailSettings.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using Ccf.Ck.Utilities.Generic;
+using System;
+using System.Collections.Generic;
 
 namespace Ccf.Ck.Models.EmailSettings
 {
@@ -9,12 +11,12 @@ namespace Ccf.Ck.Models.EmailSettings
             MailAddressesToRecipients = new List<string>();
             MailAddressesBccToRecipients = new List<string>();
         }
+        public string UserName { get; set; }
+        public string Password { get; set; }
         public string SmtpServer { get; set; }
         public int SmtpPort { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string MailAddressFrom { get; set; }
         public bool EnableSsl { get; set; }
+        public string MailAddressFrom { get; set; }
         public string MailAddressesBccTo
         {
             get { return null; }
@@ -23,7 +25,10 @@ namespace Ccf.Ck.Models.EmailSettings
                 string[] recipientsA = value.Split(new char[] { ';', ',' });
                 foreach (string item in recipientsA)
                 {
-                    MailAddressesBccToRecipients.Add(item);
+                    if (!string.IsNullOrEmpty(item))
+                    {
+                        MailAddressesBccToRecipients.Add(item);
+                    }
                 }
             }
         }
@@ -35,11 +40,28 @@ namespace Ccf.Ck.Models.EmailSettings
                 string[] recipientsA = value.Split(new char[] { ';', ',' });
                 foreach (string item in recipientsA)
                 {
-                    MailAddressesToRecipients.Add(item);
+                    if (!string.IsNullOrEmpty(item))
+                    {
+                        MailAddressesToRecipients.Add(item);
+                    }
                 }
             }
         }
+
         public List<string> MailAddressesToRecipients { get; set; }
         public List<string> MailAddressesBccToRecipients { get; set; }
+
+        public void Validate()
+        {
+            Utilities.Generic.Utilities.CheckNullOrEmpty(this, p => p.UserName, true);
+            Utilities.Generic.Utilities.CheckNullOrEmpty(this, p => p.UserName, true);
+            Utilities.Generic.Utilities.CheckNullOrEmpty(this, p => p.Password, true);
+            Utilities.Generic.Utilities.CheckNullOrEmpty(this, p => p.SmtpServer, true);
+            if (SmtpPort == 0)
+            {
+                throw new Exception($"Variable {SmtpPort} is 0 and not set");
+            }
+            Utilities.Generic.Utilities.CheckNullOrEmpty(this, p => p.MailAddressFrom, true);
+        }
     }
 }
--- Src/Utilities/Generic/Utilities.cs
+++ Src/Utilities/Generic/Utilities.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Security;
 using System.Security.Cryptography;
@@ -30,6 +31,38 @@ namespace Ccf.Ck.Utilities.Generic
             if (result && throwException)
             {
                 throw new NullReferenceException("Variable " + nameof(value) + "is null or empty");
+            }
+            return result;
+        }
+
+        public static bool CheckNullOrEmpty<TSource, TProperty>(TSource source, Expression<Func<TSource, TProperty>> propertyLambda, bool throwException)
+        {
+            Type type = typeof(TSource);
+
+            MemberExpression member = propertyLambda.Body as MemberExpression;
+            if (member == null)
+            {
+                throw new ArgumentException($"Expression '{propertyLambda.ToString()}' refers to a method, not a property.");
+            }
+            PropertyInfo propInfo = member.Member as PropertyInfo;
+            if (propInfo == null)
+            {
+                throw new ArgumentException($"Expression '{propertyLambda.ToString()}' refers to a field, not a property.");
+            }
+            if (!propInfo.ReflectedType.IsAssignableFrom(type))
+            {
+                throw new ArgumentException($"Expression '{propertyLambda.ToString()}' refers to a property that is not from type {type}.");
+            }
+
+            object value = propInfo.GetValue(source);
+            bool result = false;
+            if (value != null && value.GetType() == typeof(string))
+            {
+                result = string.IsNullOrEmpty(value as string);
+            }
+            if (result && throwException)
+            {
+                throw new NullReferenceException($"Variable {propInfo.Name} is null or empty");
             }
             return result;
         }
