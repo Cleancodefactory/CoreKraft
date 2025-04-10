66f67d762345dd084da8d9572fe48e608de89580
Robert Tanev <rtanev@cleancodefactory.de>
2025-04-10 14:42:21 +0300

    if (_STATE_PROPERTY_DONOT_CHANGE) {     if (element.ContainsKey(StatePropertyName))     {         element.Remove(StatePropertyName);     }     return; }

--- Src/SysPlugins/Utilities/DataStateUtility.cs
+++ Src/SysPlugins/Utilities/DataStateUtility.cs
@@ -8,17 +8,21 @@ namespace Ccf.Ck.SysPlugins.Utilities
     /// <summary>
     /// Imutable, singleton class
     /// </summary>
-    public class DataStateUtility: IDataStateHelper<IDictionary<string, object>> {
+    public class DataStateUtility : IDataStateHelper<IDictionary<string, object>>
+    {
         #region Construction and singleton
         private DataStateUtility() { }
 
-        public static DataStateUtility Instance {
-            get {
+        public static DataStateUtility Instance
+        {
+            get
+            {
                 return SingletonHolder.instance;
             }
         }
 
-        protected class SingletonHolder {
+        protected class SingletonHolder
+        {
             static SingletonHolder() { }
             internal static readonly DataStateUtility instance = new DataStateUtility();
         }
@@ -32,8 +36,10 @@ namespace Ccf.Ck.SysPlugins.Utilities
         public string StateDeleted { get => STATE_PROPERTY_DELETE; }
         public string StatePropertyName { get => STATE_PROPERTY_NAME; }
 
-        public string GetDataState(IDictionary<string, object> element) {
-            if (element != null && element.ContainsKey(StatePropertyName)) {
+        public string GetDataState(IDictionary<string, object> element)
+        {
+            if (element != null && element.ContainsKey(StatePropertyName))
+            {
                 return MakeValidState(element[StatePropertyName] as string);
             }
             return null;
@@ -43,15 +49,29 @@ namespace Ccf.Ck.SysPlugins.Utilities
         /// </summary>
         /// <param name="element"></param>
         /// <param name="state"></param>
-        public void SetDataState(IDictionary<string, object> element, string state) {
-            if (_STATE_PROPERTY_DONOT_CHANGE) return;
-            if (element != null) {
-                var _state = MakeValidState(state);
-                if (_state != null) {
-                    element[StatePropertyName] = _state;
-                } else if (state == null) {
+        public void SetDataState(IDictionary<string, object> element, string state)
+        {
+            if (_STATE_PROPERTY_DONOT_CHANGE)
+            {
+                if (element.ContainsKey(StatePropertyName))
+                {
                     element.Remove(StatePropertyName);
-                } else {
+                }
+                return;
+            }
+            if (element != null)
+            {
+                var _state = MakeValidState(state);
+                if (_state != null)
+                {
+                    element[StatePropertyName] = _state;
+                }
+                else if (state == null)
+                {
+                    element.Remove(StatePropertyName);
+                }
+                else
+                {
                     // wrong one - do nothing
 #if WRONG_STATE_EXCEPTIONS
                     throw new ArgumentException("The state value is incorrect.");
@@ -59,18 +79,22 @@ namespace Ccf.Ck.SysPlugins.Utilities
                 }
             }
         }
-        public bool IsDataStateOf(IDictionary<string, object> element, string state) {
-            if (state is string s && element != null && element.ContainsKey(StatePropertyName)) {
+        public bool IsDataStateOf(IDictionary<string, object> element, string state)
+        {
+            if (state is string s && element != null && element.ContainsKey(StatePropertyName))
+            {
                 return (string.Compare(s, state, false) == 0);
             }
             return false;
         }
 
-        public string GetDataState(object element) {
+        public string GetDataState(object element)
+        {
             return GetDataState(element as IDictionary<string, object>);
         }
 
-        public void SetDataState(object element, string state) {
+        public void SetDataState(object element, string state)
+        {
             SetDataState(element as IDictionary<string, object>, state);
         }
         public void SetUnchanged(object v) { SetDataState(v, StateUnchanged); }
@@ -78,13 +102,15 @@ namespace Ccf.Ck.SysPlugins.Utilities
         public void SetNew(object v) { SetDataState(v, StateNew); }
         public void SetDeleted(object v) { SetDataState(v, StateDeleted); }
 
-        public bool IsDataStateOf(object element, string state) {
+        public bool IsDataStateOf(object element, string state)
+        {
             return IsDataStateOf(element as IDictionary<string, object>, state);
         }
         #endregion
 
         #region Non-interface local helpers
-        protected string MakeValidState(string state) {
+        protected string MakeValidState(string state)
+        {
             if (STATE_VALID_VALUES.Any(s => s == state)) return state;
             return null;
         }
